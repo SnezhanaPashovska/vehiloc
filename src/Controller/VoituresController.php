7d059e67106908a9fa4e583536cb70232dfc21ee
Snezhana Pashovska <snezhana.pashovska@hotmail.com>
2024-08-29 14:06:44 +0200

    Update delete a car

--- src/Controller/VoituresController.php
+++ src/Controller/VoituresController.php
@@ -5,17 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\VoitureRepository;
 use App\Entity\Voiture;
 
 class VoituresController extends AbstractController
 {
+    private VoitureRepository $voitureRepository;
+    private EntityManagerInterface $entityManager;
 
-    public function __construct(
-        private VoitureRepository $voitureRepository,
-    )
+    public function __construct(VoitureRepository $voitureRepository, EntityManagerInterface $entityManager)
     {
-
+        $this->voitureRepository = $voitureRepository;
+        $this->entityManager = $entityManager;
     }
 
     
@@ -42,5 +44,22 @@ class VoituresController extends AbstractController
         return $this->render('voiture.html.twig', [
             'voiture' => $voiture,
         ]);
+    }
+
+    #[Route('/voiture/{id}/supprimer', name: 'app_car_delete')]
+    public function supprimer(int $id): Response 
+    {
+        $voiture = $this->voitureRepository->find($id);
+
+        if (!$voiture) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        //Supprimer la voiture
+        $this->entityManager->remove($voiture);
+        $this->entityManager->flush();
+
+        //Redirection vers la page d'accueil
+        return $this->redirectToRoute('app_home');
     }
 }
